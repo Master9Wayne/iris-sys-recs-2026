b4e120d78152309cea888546cad5677d4c1fbb62
Aradhya Mohapatra <aradhyamohapatra007@gmail.com>
2026-02-15 11:12:11 +0530

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  mount Yabeda::Prometheus::Exporter => "/metrics"
   # Defines the root path route ("/")
   root "posts#index"
 end
